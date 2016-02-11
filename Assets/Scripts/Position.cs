cc87a89281dcb27e842be6ffafc40771a9392d8a
Vivek Patel <VivekPatel@Viveks-MacBook-Pro.local>
2016-02-10 22:36:57 -0600

    Initial Push

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Position : MonoBehaviour {
+
+	public float x, y, elevation;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var newX = 0f;
+		var newY = 0f;
+		var newZ = 0f;
+
+		newX = (x - y)/2f;
+		newY = (x + y) / -4f;
+		newZ = newY;
+		if (elevation != 0) {
+			newY = newY + elevation / 4f;
+		}
+	
+
+		transform.position = new Vector3 (newX, newY, newZ);
+	}
+}
