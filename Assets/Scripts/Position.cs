cebed2f34902644c8099fea02d5a29b625a8d1f3
Todd Vrba <todd.vrba@gmail.com>
2016-02-11 07:14:54 -0500

    Change pivot of sprites to bottom.

--- Assets/Scripts/Position.cs
+++ Assets/Scripts/Position.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class Position : MonoBehaviour {
 
 	public float x, y, elevation;
-
+	
 	// Use this for initialization
 	void Start () {
 
@@ -13,18 +13,21 @@ public class Position : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		var newX = 0f;
-		var newY = 0f;
-		var newZ = 0f;
-
-		newX = (x - y)/2f;
-		newY = (x + y) / -4f;
-		newZ = newY;
-		if (elevation != 0) {
-			newY = newY + elevation / 4f;
+		if (Application.isEditor && !Application.isPlaying) {
+			Debug.Log ("Should never be seen in game made.");
+			var newX = 0f;
+			var newY = 0f;
+			var newZ = 0f;
+			
+			newX = (x - y)/2f;
+			newY = (x + y) / -4f;
+			newZ = newY;
+			if (elevation != 0) {
+				newY = newY + elevation / 4f;
+			}
+			
+			
+			transform.position = new Vector3 (newX, newY, newZ);
 		}
-	
-
-		transform.position = new Vector3 (newX, newY, newZ);
 	}
 }
