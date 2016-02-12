cfff9178550fc79fe4469d29deb0998ed70cdbac
Vivek Patel <VivekPatel@Viveks-MacBook-Pro.local>
2016-02-12 01:02:37 -0600

    added hover detection over tiles

---
+++ Assets/Scripts/MouseOverTile.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseOverTile : MonoBehaviour {
+
+	private Renderer rend;
+	private Transform trans;
+	private GameObject border;
+	// Use this for initialization
+	void Start () {
+
+		rend = GetComponent<Renderer> ();
+
+		//border.GetComponent<Renderer>().enabled = false;
+
+	}
+
+	void OnMouseEnter(){
+		trans = GetComponent<Transform> ();
+
+		border = GameObject.Find ("border");
+		//Instantiate (border);
+
+		border.GetComponent<Transform>().position = new Vector3 (trans.position.x, trans.position.y, trans.position.z - 0.1f);
+
+		//rend.material.color = Color.red;
+		border.GetComponent<Renderer>().enabled = true;
+	}
+
+	void OnMouseOver() {
+		//rend.material.color -= new Color (0.1F, 0, 0) * Time.deltaTime;
+		border.GetComponent<Renderer>().enabled = true;
+	}
+
+	void OnMouseExit(){
+		//rend.material.color = Color.white;
+		border.GetComponent<Transform>().position = new Vector3 (0f,-1f,50f);
+		border.GetComponent<Renderer>().enabled = false;
+		//borderRend.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
