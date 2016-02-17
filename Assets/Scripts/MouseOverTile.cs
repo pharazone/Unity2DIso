405eb0c17e89a07e2774f6c9f466c630ba2ef25b
Vivek Patel <VivekPatel@Viveks-MacBook-Pro.local>
2016-02-17 09:00:27 -0600

    cloning and destroying clones

--- Assets/Scripts/MouseOverTile.cs
+++ Assets/Scripts/MouseOverTile.cs
@@ -10,13 +10,12 @@ public class MouseOverTile : MonoBehaviour {
 	private bool pathSet = false;
 	private List<Tile> openTiles;
 	private List<Tile> closedTiles;
+
 	
 	// Use this for initialization
 	void Start () {
-
+		// not sure if we need this
 		rend = GetComponent<Renderer> ();
-
-		//border.GetComponent<Renderer>().enabled = false;
 
 	}
 
@@ -24,15 +23,10 @@ public class MouseOverTile : MonoBehaviour {
 		trans = GetComponent<Transform> ();
 
 		border = GameObject.Find ("border");
-		//Instantiate (border);
 
 		border.GetComponent<Transform>().position = new Vector3 (trans.position.x, trans.position.y, trans.position.z - 0.1f);
 
-		//rend.material.color = Color.red;
-		border.GetComponent<Renderer>().enabled = true;
-	}
 
-	void OnMouseOver() {
 		// We only want to show the path on mouse hover. And only once. So reset this bool when mouse exits.
 		if (!pathSet) {
 			// Get the player's tile so we can calculate shortest path to mouse tile.
@@ -41,26 +35,39 @@ public class MouseOverTile : MonoBehaviour {
 			Debug.Log ("Player is on tile " + pTile.x + ", " + pTile.y);
 			Debug.Log ("Mouse is on tile " + mTile.x + ", " + mTile.y);
 			List<Tile> adjTiles = GetAdjacentTiles(pTile);
+
 			foreach (Tile t in adjTiles) {
 				var gObj = t.GetComponent<Transform>();
-				Instantiate(border, new Vector3(gObj.position.x, gObj.position.y, gObj.position.z - 0.1f), Quaternion.identity);
+				//storing the instantiate object as GameObject in clonedBorder and giving it a unique tag
+				var clonedBorder = Instantiate (border, new Vector3(gObj.position.x, gObj.position.y, gObj.position.z - 0.1f), Quaternion.identity) as GameObject;
+				clonedBorder.tag = "clone";
+				clonedBorder.GetComponent<Renderer>().enabled = true;
 			}
 			pathSet = true;
 		}
 		border.GetComponent<Renderer>().enabled = true;
 	}
 
+	void OnMouseOver() {
+
+	}
+
 	void OnMouseExit(){
-		GameObject[] borders = GameObject.FindGameObjectsWithTag("Indicator");
-		
+		//creating an array of all the cloned borders by finding the unique tag
+		GameObject[] borders = GameObject.FindGameObjectsWithTag("clone");
+		//disabling render and destroying them (not sure why I have to disable render but it doesnt work otherwise)
 		foreach (GameObject b in borders) {
 			b.GetComponent<Transform>().position = new Vector3 (0f,-1f,50f);
 			b.GetComponent<Renderer>().enabled = false;
+			Destroy (b);
 		}
-		//rend.material.color = Color.white;
-		
+		//main border object gets repositioned and rendering turned off
+		border.GetComponent<Transform>().position = new Vector3 (0f,-1f,50f);
+		border.GetComponent<Renderer>().enabled = false;
+
+
 		pathSet = false;
-		//borderRend.enabled = false;
+
 	}
 	
 	void OnMouseDown() {
